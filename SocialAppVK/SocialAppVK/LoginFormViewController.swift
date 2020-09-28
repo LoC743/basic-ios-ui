d29f50de44a89b29d706c0fcf04be632371efe95
Alexey Morozov <alexey1562@gmail.com>
2020-09-28 20:38:16 +0300

    Добавлены метки в коде

--- SocialAppVK/SocialAppVK/LoginFormViewController.swift
+++ SocialAppVK/SocialAppVK/LoginFormViewController.swift
@@ -20,59 +20,55 @@ class LoginFormViewController: UIViewController {
         
         setupButton()
         
-        // Жест нажатия
         let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        // Присваиваем его UIScrollVIew
         scrollView?.addGestureRecognizer(hideKeyboardGesture)
+    }
+    
+    // MARK: - Настройки элементов UI
+    func setupButton() {
+        loginButton.layer.cornerRadius = 5
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     
-        // Подписываемся на два уведомления: одно приходит при появлении клавиатуры
+        // Подписка на уведомления
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
-        // Второе — когда она пропадает
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
+        // Отписка от уведомлений
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    
-    @IBAction func loginButtonPressed(_ sender: UIButton) {
-        print(#function)
-        print("Получены значения:")
-        print("Логин: \(emailPhoneTextField.text ?? "Нет значения")")
-        print("Пароль: \(passwordTextField.text ?? "Нет значения")")
-    }
-    
-    func setupButton() {
-        loginButton.layer.cornerRadius = 5
-    }
-    
+    // MARK: - Настройки использования клавиатуры
     @objc func keyboardWasShown(notification: Notification) {
-        // Получаем размер клавиатуры
         let info = notification.userInfo! as NSDictionary
         let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
         let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
         
-        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
         self.scrollView?.contentInset = contentInsets
         scrollView?.scrollIndicatorInsets = contentInsets
     }
     
-    //Когда клавиатура исчезает
     @objc func keyboardWillBeHidden(notification: Notification) {
-        // Устанавливаем отступ внизу UIScrollView, равный 0
         let contentInsets = UIEdgeInsets.zero
         scrollView?.contentInset = contentInsets
     }
     
     @objc func hideKeyboard() {
         self.scrollView?.endEditing(true)
+    }
+    
+    // MARK: - Нажатие на кнопку "Войти"
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        print(#function)
+        print("Получены значения:")
+        print("Логин: \(emailPhoneTextField.text ?? "Нет значения")")
+        print("Пароль: \(passwordTextField.text ?? "Нет значения")")
     }
 }
