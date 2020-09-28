27f24efdce5d79ed788ed0c66a228e30f96d7c0a
Alexey Morozov <alexey1562@gmail.com>
2020-09-28 18:55:26 +0300

    Создан проект SocialAppVK

---
+++ SocialAppVK/SocialAppVK/LoginFormViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginFormViewController.swift
+//  SocialAppVK
+//
+//  Created by Алексей Морозов on 28.09.2020.
+//
+
+import UIKit
+
+class LoginFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
